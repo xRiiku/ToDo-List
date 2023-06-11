7029cd54f904050ed7fb0e5ce9ffae1092fe874f
RikuDev <riku@rikudev.com>
2023-06-11 10:09:48 +0200

    edit task

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
 import './App.css';
 import { useState } from 'react';
 import trash from './assets/trash.svg'
+import pencil from './assets/pencil.svg'
 
 export default function App() {
   const [task, setTask] = useState('');
   const [allTasks, setAllTasks] = useState([]);
   const [taskId, setTaskId] = useState(1);
-  const [selected, setSelected] = useState('allTasks')
+  const [selected, setSelected] = useState('allTasks');
+  const [editValue, setEditValue] = useState('');
+  const [editingTaskId, setEditingTaskId] = useState(null);
 
   const handleChange = (event) => {
     setTask(event.target.value);
+  };
+
+  const handleEditChange = (event) => {
+    setEditValue(event.target.value);
   };
 
   const handleCheckBoxChange = (taskId) => {
@@ -31,6 +38,65 @@ export default function App() {
     setTaskId(taskId + 1);
   };
 
+  const editTask = (taskId) => {
+    setEditingTaskId(taskId);
+    setEditValue('');
+  };
+
+  const renderTask = (task) => (
+    <div key={task.id} className='task'>
+      <input
+        type="checkbox"
+        checked={task.isChecked}
+        onChange={() => handleCheckBoxChange(task.id)}
+      />
+      {task.id === editingTaskId ? (
+        <div className="editTaskInput">
+          <input
+            type="text"
+            value={editValue}
+            onChange={handleEditChange}
+          />
+          <button onClick={handleEditTask}>Edit Task</button>
+        </div>
+      ) : (
+        <span>{task.name}</span>
+      )}
+
+
+      <div className='icons'>
+        <img
+          className='trash'
+          src={trash}
+          alt='trash'
+          onClick={() => deleteTask(task.id)}
+        />
+        <img
+          className='pencil'
+          src={pencil}
+          alt='pencil'
+          onClick={() => editTask(task.id)}
+        />
+      </div>
+    </div>
+  );
+
+  let filteredTasks = allTasks;
+  if (selected === 'Completed') {
+    filteredTasks = allTasks.filter((task) => task.isChecked);
+  } else if (selected === 'Uncompleted') {
+    filteredTasks = allTasks.filter((task) => !task.isChecked);
+  }
+
+  const handleEditTask = () => {
+    const updatedTasks = allTasks.map((task) =>
+      task.id === editingTaskId ? { ...task, name: editValue } : task
+    );
+    setAllTasks(updatedTasks);
+    setEditingTaskId(null);
+    setEditValue('');
+  };
+
   return (
     <div className='parent'>
       <h1>TO-DO List</h1>
@@ -44,89 +110,21 @@ export default function App() {
           />
           <button onClick={addTask}>Add Task</button>
         </div>
-  
+
         <div className='selector'>
           <select onChange={event => setSelected(event.target.value)}>
-
             <option value='allTasks'>All tasks</option>
             <option value='Completed'>Completed</option>
             <option value='Uncompleted'>Uncompleted</option>
-
           </select>
-          
         </div>
       </div>
-  
+
       <div className='taskContainer'>
-      <div className='tasks'>
-  {selected === 'allTasks'
-    ? allTasks.map((task) => (
-        <div key={task.id} className='task'>
-          <input
-            type="checkbox"
-            checked={task.isChecked}
-            onChange={() => handleCheckBoxChange(task.id)}
-          />
-          {task.name}
-
-          <div>
-            <img
-              className='trash'
-              src={trash}
-              alt='trash'
-              onClick={() => deleteTask(task.id)}
-            />
-          </div>
+        <div className='tasks'>
+          {filteredTasks.map((task) => renderTask(task))}
         </div>
-      ))
-    : selected === 'Completed'
-    ? allTasks
-        .filter((task) => task.isChecked)
-        .map((task) => (
-          <div key={task.id} className='task'>
-            <input
-              type="checkbox"
-              checked={task.isChecked}
-              onChange={() => handleCheckBoxChange(task.id)}
-            />
-            {task.name}
-
-            <div>
-              <img
-                className='trash'
-                src={trash}
-                alt='trash'
-                onClick={() => deleteTask(task.id)}
-              />
-            </div>
-          </div>
-        ))
-    : selected === 'Uncompleted'
-    ? allTasks
-        .filter((task) => task.isChecked === false)
-        .map((task) => (
-          <div key={task.id} className='task'>
-            <input
-              type="checkbox"
-              checked={task.isChecked}
-              onChange={() => handleCheckBoxChange(task.id)}
-            />
-            {task.name}
-
-            <div>
-              <img
-                className='trash'
-                src={trash}
-                alt='trash'
-                onClick={() => deleteTask(task.id)}
-              />
-            </div>
-          </div>
-        ))
-    : null}
-</div>
-
       </div>
     </div>
   )
-}  
+}
