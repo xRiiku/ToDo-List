bfcad7a0126ae9e91293780e1a8fc2919e6d78b1
RikuDev <riku@rikudev.com>
2023-06-11 11:03:12 +0200

    edit task can cancel the edit and now you can't add an empty task

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,9 @@ export default function App() {
   const [allTasks, setAllTasks] = useState([]);
   const [taskId, setTaskId] = useState(1);
   const [selected, setSelected] = useState('allTasks');
-  const [editValue, setEditValue] = useState('');
-  const [editingTaskId, setEditingTaskId] = useState(null);
+  const [editValue, setEditValue] = useState(''); //Estado para mostrar el valor anterior y posterior a la edición
+  const [editingTaskId, setEditingTaskId] = useState(null); //Estado para identificar el id de la tarea que se está editando
+  const [editing, setEditing] = useState(false); //Estado para identificar si se está editando una tarea o no
 
   const handleChange = (event) => {
     setTask(event.target.value);
@@ -20,27 +21,48 @@ export default function App() {
   };
 
   const handleCheckBoxChange = (taskId) => {
-    setAllTasks((prevTasks) =>
-      prevTasks.map((task) =>
+    setAllTasks((allTasks) =>
+      allTasks.map((task) =>
         task.id === taskId ? { ...task, isChecked: !task.isChecked } : task
+        /* Recorremos el array y comprobamos en cada iteración si la tarea está marcada como completada o no. Si estaba marcada como completada, se marcará como no completada y viceversa*/
       )
     );
   };
 
   const deleteTask = (taskId) => {
     setAllTasks(allTasks.filter((task) => task.id !== taskId));
+    /* El método filter() devuelve un nuevo array, en la cual excluimos la tarea con el id correspondiente al id que le pasamos por parámetro*/
   };
 
   const addTask = () => {
-    const newTask = { id: taskId, name: task, isChecked: false };
-    setAllTasks([...allTasks, newTask]);
-    setTask('');
-    setTaskId(taskId + 1);
+    if (task.trim() !== '') { //El método trim() elimina espacios en blanco. Si no hay ningún espacio en blanco, entonces añade la tarea
+      const newTask = { id: taskId, name: task, isChecked: false };
+      setAllTasks([...allTasks, newTask]);
+      setTask('');
+      setTaskId(taskId + 1);
+    }
+  };
+  
+
+  const editTask = (taskId, taskName) => {
+    setEditingTaskId(taskId); //Estado para identificar la tarea que se está editando
+    setEditValue(taskName); // muestra la tarea anterior como valor inicial en el input
+    setEditing(true); // Estado para determinar si estamos editando la tarea o no.
   };
 
-  const editTask = (taskId) => {
-    setEditingTaskId(taskId);
+  const cancelEditTask = () => {
+    setEditingTaskId(null);
     setEditValue('');
+    setEditing(false);
+  };
+
+  const handleEditTask = () => {
+    const updatedTasks = allTasks.map((task) =>
+      task.id === editingTaskId ? { ...task, name: editValue } : task
+      /* Comprobamos si la ID de la tarea coincide con la tarea que está editando actualmente (editingTaskId), si coincide, devuelve el objeto con la tarea anterior, con la opción de modificarlo / editarlo. Si el ID no coincide, devuelve la tarea sin cambios*/
+    );
+    setAllTasks(updatedTasks);
+    cancelEditTask();
   };
 
   const renderTask = (task) => (
@@ -50,7 +72,7 @@ export default function App() {
         checked={task.isChecked}
         onChange={() => handleCheckBoxChange(task.id)}
       />
-      {task.id === editingTaskId ? (
+      {task.id === editingTaskId && editing ? (
         <div className="editTaskInput">
           <input
             type="text"
@@ -58,11 +80,12 @@ export default function App() {
             onChange={handleEditChange}
           />
           <button onClick={handleEditTask}>Edit Task</button>
+          <button onClick={cancelEditTask}>Cancel</button>
         </div>
       ) : (
         <span>{task.name}</span>
       )}
-
+      {/* Si la Id de la tarea coincide con la tarea que se está editando (editingTaskID) y además se está editando dicha tarea, entonces muestra un input con el valor de la tarea anterior el cual se puede modificar. Si no se está editando, muestra la tarea. */}
 
       <div className='icons'>
         <img
@@ -75,7 +98,7 @@ export default function App() {
           className='pencil'
           src={pencil}
           alt='pencil'
-          onClick={() => editTask(task.id)}
+          onClick={() => editTask(task.id, task.name)}
         />
       </div>
     </div>
@@ -87,15 +110,6 @@ export default function App() {
   } else if (selected === 'Uncompleted') {
     filteredTasks = allTasks.filter((task) => !task.isChecked);
   }
-
-  const handleEditTask = () => {
-    const updatedTasks = allTasks.map((task) =>
-      task.id === editingTaskId ? { ...task, name: editValue } : task
-    );
-    setAllTasks(updatedTasks);
-    setEditingTaskId(null);
-    setEditValue('');
-  };
 
   return (
     <div className='parent'>
